5d870cbe23dfaedb9dde2ba0d1cdf177a74418fc
eisirame <eisirame@LFR022705.corp.capgemini.com>
2018-04-11 21:45:45 +0200

    # WARNING: head commit changed in the meantime
    merge
    
    Change-Id: I50b1eedf8be89da13d85c318660368269fa387bc
    Signed-off-by: eisirame <eisirame@LFR022705.corp.capgemini.com>

--- ApiManga/src/com/api/manga/restserver/entities/Artist.java
+++
@@ -1,8 +0,0 @@
-package com.api.manga.restserver.entities;
-
-public class Artist {
-	
-	private int id;
-	private String name;
-
-}
--- ApiManga/src/com/api/manga/restserver/entities/Status.java
+++
@@ -1,8 +0,0 @@
-package com.api.manga.restserver.entities;
-
-public class Status {
-	
-	private int id;
-	private String name;
-
-}
